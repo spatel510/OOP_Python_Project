0b08d3304887e025c08096c786f9fc7bcd3f6cb1
Shraddha Patel <shr.patel1996@gmail.com>
2022-10-02 12:11:52 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from re import I
+
+
+class Item:
+    def calculate_total_price(self):
+        
+        
+
+item1 = Item()
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
+
+item2 = Item()
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3
