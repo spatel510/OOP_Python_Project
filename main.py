4e64d8982e430f135b4bf83e60a3eda06a7a4133
Shraddha Patel <shr.patel1996@gmail.com>
2022-10-03 10:48:47 -0500

    change to main

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from re import I
 
-
 class Item:
-    def calculate_total_price(self):
-        
-        
+    def __init__(self, name):
+        print(f"{name}")
+    def calculate_total_price(self, x, y):
+        return x * y
 
-item1 = Item()
-item1.name = "Phone"
+
+item1 = Item("Phone")
+
 item1.price = 100
 item1.quantity = 5
+item1.total_price = item1.calculate_total_price(item1.price, item1.quantity)
+print(item1.total_price)
 
-item2 = Item()
-item2.name = "Laptop"
+item2 = Item("Laptop")
+
 item2.price = 1000
 item2.quantity = 3
